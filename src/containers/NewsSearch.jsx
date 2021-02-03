57c68e9c3ec6e19a50d09c32254307fb3866d3f6
dl-watson <dlarkinwatson@gmail.com>
2021-02-03 14:10:52 -0800

    conditional rendering for loading; pass articles to ArticlesList via props

--- src/containers/NewsSearch.jsx
+++ src/containers/NewsSearch.jsx
@@ -1,19 +1,24 @@
 import React, { Component } from "react";
+import ArticlesList from "../components/articles/ArticlesList";
 import getArticles from "../services/getArticles";
 
 export default class NewsSearch extends Component {
   state = {
-    loading: false,
+    loading: true,
     articles: [],
     search: "",
   };
 
   componentDidMount() {
-    return getArticles().then(({ articles }) => this.setState({ articles }));
+    return getArticles().then(({ articles }) =>
+      this.setState({ articles, loading: false })
+    );
   }
 
   render() {
-    const { articles } = this.state;
-    return <div></div>;
+    const { articles, loading } = this.state;
+    return (
+      <>{loading ? <>Loading...</> : <ArticlesList articles={articles} />}</>
+    );
   }
 }
