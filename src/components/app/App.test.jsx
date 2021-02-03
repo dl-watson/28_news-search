6f11105ba5a946edfbae3e3919a61cbb2487b667
dl-watson <dlarkinwatson@gmail.com>
2021-02-03 15:19:39 -0800

    set up Article test

--- src/components/app/App.test.jsx
+++ src/components/app/App.test.jsx
@@ -1,7 +1,6 @@
 import React from "react";
 import { render, cleanup } from "@testing-library/react";
 import App from "./App";
-import fetch from "node-fetch";
 
 describe("App component", () => {
   afterEach(() => cleanup());
