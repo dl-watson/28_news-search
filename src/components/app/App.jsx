015e3c27c8f58d483b9d475242cd9a0cf6a8d649
dl-watson <dlarkinwatson@gmail.com>
2021-02-03 13:31:51 -0800

    set up router

--- src/components/app/App.jsx
+++ src/components/app/App.jsx
@@ -1,5 +1,22 @@
-import React from 'react';
+import React from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
-export default function App() {
-  return <h1>Hello World</h1>;
+export default class App extends React.Component {
+  render() {
+    return (
+      <>
+        <div className="main">
+          <Router>
+            <Switch>
+              <Route
+                path="/"
+                exact
+                render={(routerProps) => <NewsSearch {...routerProps} />}
+              />
+            </Switch>
+          </Router>
+        </div>
+      </>
+    );
+  }
 }
