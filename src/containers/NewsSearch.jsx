90b2c0fb4f3356f9e3279a8028c3ce00e4360753
dl-watson <dlarkinwatson@gmail.com>
2021-02-03 14:07:39 -0800

    articles working as expected

--- src/containers/NewsSearch.jsx
+++ src/containers/NewsSearch.jsx
@@ -1,7 +1,19 @@
 import React, { Component } from "react";
+import getArticles from "../services/getArticles";
 
 export default class NewsSearch extends Component {
+  state = {
+    loading: false,
+    articles: [],
+    search: "",
+  };
+
+  componentDidMount() {
+    return getArticles().then(({ articles }) => this.setState({ articles }));
+  }
+
   render() {
+    const { articles } = this.state;
     return <div></div>;
   }
 }
